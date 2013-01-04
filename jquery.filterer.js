9d451480765661f7472324f4c323a01446ae4b27
Joël Cox <joel@joelcox.nl>
2013-01-04 11:59:23 +0100

    Added option for highlighting the search term.

--- jquery.filterer.js
+++ jquery.filterer.js
@@ -8,18 +8,23 @@
 
 	$.fn.filterer = function(items, fields, options) {
 
-		var defaults = {};
+		var defaults = {
+			'highlight': false
+		};
+
 		options = $.extend(defaults, options);
 
 		return this.each(function() {
 
 			$(this).on('keyup', function(e) {
-				var searchVal = $(this).val().toLowerCase();
+				var searchTerm = $(this).val();
 
 				// Make sure everything is visible if the search
 				// term is empty
-				if (searchVal === '') {
+				if (searchTerm === '') {
+					if (options.highlight) removeStrongWrap(items, fields);
 					$(items).css('display', 'block');
+
 					return false;
 				}
 
@@ -32,12 +37,19 @@
 					// See if the search val is found in one of the
 					// data elements
 					$.each(fields, function(index, el) {
-						var text = $(record).find(el).text().toLowerCase();
+						var text = $(record).find(el).text();
 
-						if (text.indexOf(searchVal) >= 0) {
-							showRecord = true;
-							return false;
+						// Set up the regular expresion, case-insensitive
+						var regexp = new RegExp(searchTerm, 'gi');
+						var matches = text.match(regexp);
+						if (matches) showRecord = true;
+
+						// Highlight search terms if required
+						if (matches && options.highlight) {
+							var highlightedText = highlightSearchTerm(text, matches);
+							$(record).find(el).html(highlightedText);
 						}
+
 					});
 
 					if (showRecord) recordEl.css('display', 'block');
@@ -47,6 +59,38 @@
 			});
 
 		});
+
+		/**
+		 * Wraps the search term in a strong element
+		 */
+		function highlightSearchTerm(text, matches) {
+			$.each(matches, function(index, match) {
+				text = text.replace(match, '<strong>' + match + '</strong>');
+			});
+
+			return text;
+		}
+
+		/**
+		 * Remove strong element from items
+		 */
+		function removeStrongWrap(items, fields)
+		{
+			$.each($(items), function(index, record) {
+				var $record = $(record);
+
+				// Loop over the fields, turn the HTML into plaintext
+				// and then replace the HTML for the plaintext.
+				$.each(fields, function(index, field) {
+					$field = $record.find(field);
+
+					var plainText = $field.text();
+					$field.html(plainText);
+				});
+
+			});
+
+		}
 
 	};
 
